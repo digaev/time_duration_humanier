0d4358a6cb3b42b79d2ea29f11795ed6d78cd854
Nikolay Digaev <ffs.cmp@gmail.com>
2015-12-11 16:48:57 +0500

    Remove double spaces

--- test/time_duration_humanizer_test.rb
+++ test/time_duration_humanizer_test.rb
@@ -2,37 +2,37 @@ require 'test_helper'
 
 class TimeDurationHumanizerTest < TimeDurationHumanizer::Test
   def test_singular_time
-    assert_equal '1 hour, 1 minute and 1 second',  TimeDurationHumanizer.humanize(3661)
-    assert_equal '1 hour, 1 minute, 1 second',  TimeDurationHumanizer.humanize(3661, { and_at_end: false })
+    assert_equal '1 hour, 1 minute and 1 second', TimeDurationHumanizer.humanize(3661)
+    assert_equal '1 hour, 1 minute, 1 second', TimeDurationHumanizer.humanize(3661, { and_at_end: false })
   end
 
   def test_plural_time
-    assert_equal '2 hours, 2 minutes and 2 seconds',  TimeDurationHumanizer.humanize(7322)
-    assert_equal '2 hours, 2 minutes, 2 seconds',  TimeDurationHumanizer.humanize(7322, { and_at_end: false })
+    assert_equal '2 hours, 2 minutes and 2 seconds', TimeDurationHumanizer.humanize(7322)
+    assert_equal '2 hours, 2 minutes, 2 seconds', TimeDurationHumanizer.humanize(7322, { and_at_end: false })
   end
 
   def test_singular_date
-    assert_equal '1 year, 1 month and 1 day',  TimeDurationHumanizer.humanize(34236000)
-    assert_equal '1 year, 1 month, 1 day',  TimeDurationHumanizer.humanize(34236000, { and_at_end: false })
+    assert_equal '1 year, 1 month and 1 day', TimeDurationHumanizer.humanize(34236000)
+    assert_equal '1 year, 1 month, 1 day', TimeDurationHumanizer.humanize(34236000, { and_at_end: false })
   end
 
   def test_plural_date
-    assert_equal '2 years, 2 months and 2 days',  TimeDurationHumanizer.humanize(68472000)
-    assert_equal '2 years, 2 months, 2 days',  TimeDurationHumanizer.humanize(68472000, { and_at_end: false })
+    assert_equal '2 years, 2 months and 2 days', TimeDurationHumanizer.humanize(68472000)
+    assert_equal '2 years, 2 months, 2 days', TimeDurationHumanizer.humanize(68472000, { and_at_end: false })
   end
 
   def test_weeks
-    assert_equal '7 days',  TimeDurationHumanizer.humanize(604800)
-    assert_equal '14 days',  TimeDurationHumanizer.humanize(1209600)
+    assert_equal '7 days', TimeDurationHumanizer.humanize(604800)
+    assert_equal '14 days', TimeDurationHumanizer.humanize(1209600)
 
-    assert_equal '1 week',  TimeDurationHumanizer.humanize(604800, {}, { weeks: true })
-    assert_equal '2 weeks',  TimeDurationHumanizer.humanize(1209600, {}, { weeks: true })
+    assert_equal '1 week', TimeDurationHumanizer.humanize(604800, {}, { weeks: true })
+    assert_equal '2 weeks', TimeDurationHumanizer.humanize(1209600, {}, { weeks: true })
 
-    assert_equal '1 year, 1 month and 8 days',  TimeDurationHumanizer.humanize(34840800)
-    assert_equal '1 year, 1 month, 1 week and 1 day',  TimeDurationHumanizer.humanize(34840800, {}, { weeks: true })
+    assert_equal '1 year, 1 month and 8 days', TimeDurationHumanizer.humanize(34840800)
+    assert_equal '1 year, 1 month, 1 week and 1 day', TimeDurationHumanizer.humanize(34840800, {}, { weeks: true })
 
-    assert_equal '1 year, 1 month and 15 days',  TimeDurationHumanizer.humanize(35445600)
-    assert_equal '1 year, 1 month, 2 weeks and 1 day',  TimeDurationHumanizer.humanize(35445600, {}, { weeks: true })
+    assert_equal '1 year, 1 month and 15 days', TimeDurationHumanizer.humanize(35445600)
+    assert_equal '1 year, 1 month, 2 weeks and 1 day', TimeDurationHumanizer.humanize(35445600, {}, { weeks: true })
   end
 
   def test_days_in_years
